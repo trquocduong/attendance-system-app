49ec7cb2d5b674ac20fbdb95a53bd8e7a1c0363b
tranquocduong <quocduong0338542631@gmail.com>
2026-05-08 22:27:51 +0700

    day 2 basic get post api

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# fake database user
+users = []
+
+
+# Pydanti schema
+class UserCreate(BaseModel):
+    name : str
+    email:str
+    age:int
+
+
+
 @app.get("/")
 def home():
     return { "message ": "Start Attendace App"}
-@app.get("/hello")
+@app.get("/users")
 def hello():
-    return {"message": "Heloo Duong"}
+    return users
+@app.post("/users")
+def create_user(user:UserCreate):
+    users.append(user.dict())
+    
+    return {
+        "message":"User created successfully",
+        "data":user
+    }
