eec457afd793ed46469620e2d829fd70f5593d8f
trquocduong <quocduong0338542631@gmail.com>
2026-05-16 22:37:26 +0700

    day 8 desktop login ui with customtkinter

---
+++ desktop/main.py
@@ -0,0 +1,61 @@
+import customtkinter as ctk
+
+
+# setup theme
+ctk.set_appearance_mode("dark")
+
+
+# tạo app window
+app = ctk.CTk()
+
+# title
+app.title("Attendance System")
+
+# width x height
+app.geometry("500x400")
+
+# title label form login
+title_label = ctk.CTkLabel(
+    app,
+    text="Login",
+    font=("Arial", 28)
+)
+title_label.pack(pady=10)
+# username input
+username_entry = ctk.CTkEntry(
+    app,
+    placeholder_text="Username",
+    width=300
+)
+
+username_entry.pack(pady=10)
+# password input
+password_entry = ctk.CTkEntry(
+    app,
+    placeholder_text="Password",
+    show="*",
+    width=300
+)
+
+password_entry.pack(pady=10)
+
+
+def handle_login():
+
+    username = username_entry.get()
+
+    password = password_entry.get()
+
+    print(username)
+    print(password)
+
+
+login_button = ctk.CTkButton(
+    app,
+    text="Login",
+    command=handle_login
+)
+
+login_button.pack(pady=20)
+# chạy app
+app.mainloop()
